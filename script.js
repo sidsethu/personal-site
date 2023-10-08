6224cf8b41b6686569fba349c0cce34c1b71e019
sidsethu <sid.sethu@gmail.com>
2023-10-09 00:49:14 +0530

    Some Styling Fixes

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+// Get the elements you need
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body; // You can also use document.documentElement to target the <html> element
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+  if (darkModeToggle.checked) {
+    body.classList.add('dark-mode');
+  } else {
+    body.classList.remove('dark-mode');
+  }
+}
+
+// Add an event listener to the toggle switch
+darkModeToggle.addEventListener('change', toggleDarkMode);
+
+// Call toggleDarkMode initially based on user preference (if stored)
+const isDarkModePreferred = localStorage.getItem('darkMode');
+if (isDarkModePreferred === 'true') {
+  darkModeToggle.checked = true;
+  toggleDarkMode();
+}
